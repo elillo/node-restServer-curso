bf30cbcfbaa21d25391de2f08b2f1ddcc802c9e8
elillo <elilloperez35@gmail.com>
2018-06-19 18:54:52 +0100

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,24 @@
 //=================================
 
 process.env.PORT = process.env.PORT || 3000;
+
+//=================================
+//         --Entorno--
+//=================================
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
+
+//=================================
+//         --Base de datos--
+//=================================
+let urlDB;
+
+ if(process.env,NODE_ENV === "dev"){
+
+    urldb = "mongodb://localhost:27017/cafe";
+
+}else{
+    urlDB = "mongodb://user_cafe:curso102@ds261470.mlab.com:61470/bebida";
+ }
+
+process.env.URLDB = urlDB;
+
